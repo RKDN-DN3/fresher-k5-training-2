b530a2a9c44d2ed448bbb0ec83d06e71ad640ade
Trần Hoàng Anh <hoanganhtran669@gmail.com>
2021-08-25 16:51:40 +0700

    fix ui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,36 @@
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">TODO APP</a>
-        <form class="d-flex">
-          <div v-if="loggedIn" style="color: #00000; display: inline">
-            <b>Xin chào {{getUser}}</b>
-            <button class="btn btn-outline-danger" type="button" value="Logout" style="margin-left: 5px" @click="logout" >LogOut</button>
+        <div
+          class="collapses navbar-collapses">
+          <div class=" navbar-nav" style="position: relative; left: 30px;">
+            <router-link to="/task-list" class="nav-link"
+              >Task List</router-link
+            >
           </div>
-          <div v-else>
-            <button class="btn btn-outline-success" type="button"><a class="nav" href="/login">Login</a></button>
-            <button class="btn btn-outline-warning" type="button"><a class="nav" href="/signup">Register</a></button>
-          </div>
-        </form>
+          <form class="d-flex"  style="position: absolute; right: 15px">
+            <div v-if="loggedIn" style="color: #00000; display: inline">
+              <b>Xin chào {{ getUser }}</b>
+              <button
+                class="btn btn-outline-danger"
+                type="button"
+                value="Logout"
+                style="margin-left: 5px"
+                @click="logout"
+              >
+                LogOut
+              </button>
+            </div>
+            <div v-else>
+              <button class="btn btn-outline-success" type="button">
+                <a class="nav" href="/login">Login</a>
+              </button>
+              <button class="btn btn-outline-warning" type="button">
+                <a class="nav" href="/signup">Register</a>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </nav>
   </div>
@@ -25,13 +45,10 @@ export default {
     ...mapGetters(["loggedIn", "getUser"]),
   },
   methods: {
-    
     logout() {
-      this.$store.dispatch("logout").then(()=>this.$router.replace('/login'));
-      
+      this.$store.dispatch("logout").then(() => this.$router.replace("/login"));
     },
   },
-   
 };
 </script>
 
@@ -39,15 +56,22 @@ export default {
 .header {
   margin-bottom: 10px;
 }
-a.nav{
-    color: #b797e4;
-    text-decoration: none;
+a.nav {
+  color: #b797e4;
+  text-decoration: none;
 }
-a.nav:hover{
-    color: #eeecf1;
-    
+a.nav:hover {
+  color: #eeecf1;
 }
 b {
-    color: #eeecf1;
+  color: #eeecf1;
+}
+.collapses {
+  display: inherit;
+}
+.navbar-collapses {
+  flex-basis: 50%;
+  flex-grow: 1;
+  align-items: center;
 }
 </style>
